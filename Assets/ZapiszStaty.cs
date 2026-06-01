f239da00925434c17c785233e4d295b490880f3d
Magdolla <magdalenaczech410@gmail.com>
2026-06-01 21:44:50 +0200

    walka

--- Assets/ZapiszStaty.cs
+++ Assets/ZapiszStaty.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 
-public class ZapiszStaty : MonoBehaviour
+
+public class GameManager : MonoBehaviour
 {
-    public static ZapiszStaty instance;
-    public int PlayerHealth = 100;
+    // Statyczna instancja, która pozwala na dostęp do skryptu z innych obiektów
+    public static GameManager instance;
+
+    [Header("Statystyki Gracza")]
+    public int lives = 0;
+    public int coins = 0;
 
     private void Awake()
     {
-        if (instance != null && instance != this)
+        // Sprawdzamy, czy instancja już istnieje
+        if (instance != null)
         {
+            // Jeśli tak, niszczymy nowy obiekt, aby zachować tylko ten oryginalny
             Destroy(gameObject);
-            return;
         }
-        instance = this;
+        else
+        {
+            // Jeśli to pierwszy obiekt, ustawiamy go jako instancję
+            instance = this;
+            // Zapewniamy, że obiekt nie zostanie zniszczony przy zmianie sceny
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
